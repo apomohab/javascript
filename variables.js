336662ab8eb43a9e8d3fc32967e4579cc1e45898
Saeed Shaban <42922808+apomohab@users.noreply.github.com>
2019-06-03 05:24:23 +0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,11 @@
+// First me exersize on 3-6-2019
+
+var Name = ' Saeed Shaban';
+var Age  = 26;
+var Country = 'Egypt';
+var live    =  ' Saudi Arabia';
+var Learned = 'Full Stack Developer';
+var School  = 'Udacity';
+
+
+console.log(' my name is ' + Name + ' And my age is ' + Age + ' Im From ' + Country + ' but live in ' + live + ' Im Learned ' + Learned + ' in the school ' + School) 
